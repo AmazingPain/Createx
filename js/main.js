17730e1e17e618ed786de35b78054aec9b40ab62
Иван Серяк <seryak.97@mail.ru>
2023-01-03 18:06:46 +0300

    added first slider

--- js/main.js
+++ js/main.js
@@ -5,5 +5,23 @@ $(function () {
     $('.directions__filter-btn').removeClass('directions__filter-btn--active')
     $(this).addClass('directions__filter-btn--active')
   })
-})
 
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+  })
+
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
+
+})
